bfd8fe92fd6c11676de37dc4bcb173cee390aae6
Arnab Ghose <arnab.ghose128@gmail.com>
2025-11-17 11:39:25 +0530

    added offline verify logic as a standalone function in order to enable counterparty verification

--- examples/crypto.py
+++ examples/crypto.py
@@ -1,9 +1,20 @@
-from rubix.crypto.secp256k1 import Secp256k1Keypair
+from rubix.client import RubixClient
+from rubix.signer import Signer
+from rubix.crypto.secp256k1 import Secp256k1Keypair, secp256k1_verify
 
-def sign_arbitrary_data(data: bytes):
+def sign_and_verify_arbitrary_data(data: bytes):
     # Generate a new Secp256k1 keypair
-    keypair = Secp256k1Keypair.from_private_key(bytes.fromhex("<private key in hex>"))
+    client = RubixClient("<Rubix Node URL>")
 
-    print("Public Key (hex):", keypair.public_key)
+    signer = Signer(
+        rubixClient=client,
+        mnemonic="<Enter 24-word long BIP-39 mnemonic>"
+    )
+
+    print("Public Key (hex): ", signer.get_keypair().public_key)
+    keypair = signer.get_keypair()
 
     signature_bytes = keypair.sign(data)
+
+    is_valid = secp256k1_verify(bytes.fromhex(keypair.public_key), data, signature_bytes)
+    print("Is the signature valid?: ", is_valid)
--- rubix/crypto/secp256k1.py
+++ rubix/crypto/secp256k1.py
@@ -1,11 +1,64 @@
 import bip32utils
-import hashlib
-import base64
 
-from ecpy.curves import Curve
-from ecpy.keys import ECPrivateKey
+from ecpy.curves import Curve, Point
+from ecpy.keys import ECPrivateKey, ECPublicKey
 from ecpy.ecdsa import ECDSA
-from ecdsa import SigningKey, SECP256k1, util
+from ecdsa import SigningKey, SECP256k1
+from coincurve import PublicKey as CoincurvePublicKey
+
+def secp256k1_sign(private_key: bytes, message: bytes) -> bytes:
+    """Signs a message using secp256k1 private key.
+
+    Args:
+        private_key (bytes): Secp256k1 private key.
+        message (bytes): The message to sign.
+
+    Returns:
+        bytes: The generated signature in bytes.
+    """
+    cv = Curve.get_curve('secp256k1')
+    pv_key = ECPrivateKey(int.from_bytes(private_key, 'big'), cv)
+    signer = ECDSA()
+
+    sig = signer.sign(message, pv_key)
+    if sig is None:
+        raise ValueError("Failed to sign the message.")
+    
+    return bytes(sig)
+
+def secp256k1_verify(public_key: bytes, message: bytes, signature: bytes) -> bool:
+    """Verifies secp256k1 signature.
+
+    Args:
+        public_key (bytes): Compressed public key.
+        message (bytes): The original message that was signed.
+        signature (bytes): The signature to verify.
+
+    Returns:
+        bool: True if signature is valid, False otherwise.
+    """
+    cv = Curve.get_curve('secp256k1')
+
+    # check if the public key is in compressed format
+    uncompressed_public_key = None
+    
+    if len(public_key) == 33:
+        coincurve_pub_key = CoincurvePublicKey(public_key)
+        uncompressed_public_key = coincurve_pub_key.format(compressed=False)
+    elif len(public_key) == 65:
+        uncompressed_public_key = public_key
+    else:
+        raise ValueError(f"Invalid public key of length {len(public_key)}.")
+    
+    # Form public key object
+    x = int.from_bytes(uncompressed_public_key[1:33], 'big')
+    y = int.from_bytes(uncompressed_public_key[33:], 'big')
+    ec_point = Point(x, y, cv)
+    pub_key_obj = ECPublicKey(ec_point)
+
+    # Verify signature
+    verifier = ECDSA()
+    return verifier.verify(message, signature, pub_key_obj)
 
 class Secp256k1Keypair:
     def __init__(self, private_key: str, public_key: str):
@@ -68,33 +121,4 @@ class Secp256k1Keypair:
         Returns:
             bytes: The generated signature in bytes.
         """
-        cv = Curve.get_curve('secp256k1')
-        pv_key = ECPrivateKey(int(self.__private_key, 16), cv)
-        signer = ECDSA()
-
-        sig = signer.sign(message, pv_key)
-        if sig is None:
-            raise ValueError("Failed to sign the message.")
-        
-        return sig
-
-    def verify(self, message: bytes, signature: bytes) -> bool:
-        """Verifies secp256k1 signature.
-
-        Args:
-            message (bytes): The original message that was signed.
-            signature (bytes): The signature to verify.
-
-        Returns:
-            bool: True if signature is valid, False otherwise.
-        """
-        cv = Curve.get_curve('secp256k1')
-        priv_key = ECPrivateKey(int(self.__private_key, 16), cv)
-
-        pub_key = priv_key.get_public_key()
-        signer = ECDSA()
-        
-        try:
-            return signer.verify(message, signature, pub_key)
-        except Exception:
-            return False
+        return secp256k1_sign(bytes.fromhex(self.__private_key), message)
